67550b9bd1e36d87143124e2efe409b0db5218ae
musicbender <musicbender@gmail.com>
2015-12-07 16:24:11 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    var context = new AudioContext(),
+        getSound = new XMLHttpRequest(),
+        snare;
+
+    getSound.open("GET", "snare.mp3", true);
+    getSound.responseType = 'arraybuffer';
+
+    getSound.onload = function() {
+        context.decodeAudioData(getSound.response, function(buffer) {
+        snare = buffer;
+        });
+    }
+
+    getSound.send();
+    
+    $('#sub-btn').click(function() {
+        var playSound = context.createBufferSource();
+        playSound.buffer = snare;
+        playSound.connect(context.destination);
+        playSound.start(0);
+    });
+});
+
+
+//Checking on the server stuff
+    /*getSound.onreadystatechange = function () {
+        if (getSound.readyState == 4 && getSound.status == 200) {
+            $('#scores').html(getSound.responseText);
+        }
+        else {
+            $('#scores').html('<p>Waiting for Server Response...' + getSound.status + getSound.readyState + '</p>');
+        }
+    }*/
+    
+    
+    
+    
+    
